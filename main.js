fcf4aeac9cadd618b2220713498564979c239b23
Vlerry <valerialuca.v@gmail.com>
2023-01-21 20:58:42 +0100

    Added theme css, update Modal layout, updated table UI.

--- main.js
+++ main.js
@@ -1,100 +1,106 @@
-import { eventState, addSportEvent } from './state.js';
-import { createDetailsModal } from './templates/detailsModal.js';
+import { eventState, addSportEvent } from "./state.js";
+import { createDetailsModal } from "./templates/detailsModal.js";
 import { tableRowEvents } from "./utils/templateUtils.js";
-import { createTableRow } from './templates/tableRow.js';
+import { createTableRow } from "./templates/tableRow.js";
 
-const tableBody = document.getElementById('tbody');
-const detailsContainer = document.getElementById('details-container');
-const content = document.getElementById('content');
+const tableBody = document.getElementById("tbody");
+const detailsContainer = document.getElementById("details-container");
+const content = document.getElementById("content");
 
 tableBody.append(...tableRowEvents);
 
-
 const onTableBodyClick = (event) => {
-    const selectedIndex = event.target.closest('tr[data-id]').getAttribute('data-id');
-    const eventModal = createDetailsModal(eventState[selectedIndex]);
+  const selectedIndex = event.target
+    .closest("tr[data-id]")
+    .getAttribute("data-id");
+  const eventModal = createDetailsModal(eventState[selectedIndex]);
 
-    content.innerHTML = eventModal;
+  content.innerHTML = eventModal;
 
-    detailsContainer.classList.add("show");
+  detailsContainer.classList.add("show");
 
-    detailsContainer.querySelector(".back-drop").addEventListener("click", () => {
-        detailsContainer.classList.remove("show");
-        content.innerHTML = "";
-    });
+  detailsContainer.querySelector(".back-drop").addEventListener("click", () => {
+    detailsContainer.classList.remove("show");
+    content.innerHTML = "";
+  });
+};
 
-}
-
-tableBody.addEventListener('click', onTableBodyClick);
-
+tableBody.addEventListener("click", onTableBodyClick);
 
 // Dialog window
 
-const addEventButton = document.getElementById('add-new-event-btn');
-const addEventDialog = document.getElementById('add-new-event');
+const addEventButton = document.getElementById("add-new-event-btn");
+const addEventDialog = document.getElementById("add-new-event");
 
-
-addEventButton.addEventListener('click', () => {
-    addEventDialog.showModal();
+addEventButton.addEventListener("click", () => {
+  addEventDialog.showModal();
 });
 
 const mapResultFromSubmitToObject = (elements) => {
-    return {
+  return {
+    season: elements.season.value,
+    status: elements.status.value,
+    timeVenueUTC: elements.timeVenueUTC.value,
+    dateVenue: elements.dateVenue.value,
+    homeTeam: {
+      officialName: elements.homeTeamName.value,
+      slug: elements.homeTeamSlug.value,
+      abbreviation: elements.homeTeamNameAbbreviation.value,
+      teamCountryCode: elements.homeTeamCountryCode.value,
+    },
 
-        season: elements.season.value,
-        status: elements.status.value,
-        timeVenueUTC: elements.timeVenueUTC.value,
-        dateVenue: elements.dateVenue.value,
-        homeTeam: {
-            officialName: elements.homeTeamName.value,
-            slug: elements.homeTeamSlug.value,
-            abbreviation: elements.homeTeamNameAbbreviation.value,
-            teamCountryCode: elements.homeTeamCountryCode.value
-        },
+    awayTeam: {
+      officialName: elements.awayTeamName.value,
+      slug: elements.awayTeamSlug.value,
+      abbreviation: elements.awayTeamNameAbbreviation.value,
+      teamCountryCode: elements.awayTeamCountryCode.value,
+    },
+    result: {
+      homeGoals: elements.homeTeamGoals.value,
+      awayGoals: elements.awayTeamGoals.value,
+      winner: elements.winner.value,
+      yellowCards: elements.yellowCards.value,
+      secondYellowCards: elements.secondYellowCards.value,
+      directRedCards: elements.directRedCards.value,
+    },
+    stage: {
+      name: elements.stageName.value,
+      ordering: elements.stageOrdering.value,
+    },
 
-        awayTeam: {
-            officialName: elements.awayTeamName.value,
-            slug: elements.awayTeamSlug.value,
-            abbreviation: elements.awayTeamNameAbbreviation.value,
-            teamCountryCode: elements.awayTeamCountryCode.value
-        },
-        result: {
-            homeGoals: elements.homeTeamGoals.value,
-            awayGoals: elements.awayTeamGoals.value,
-            winner: elements.winner.value,
-            yellowCards: elements.yellowCards.value,
-            secondYellowCards: elements.secondYellowCards.value,
-            directRedCards: elements.directRedCards.value
-        },
-        stage: {
-            name: elements.stageName.value,
-            ordering: elements.stageOrdering.value
-        },
+    originCompetitionName: elements.originCompetitionName.value,
+  };
+};
 
-        originCompetitionName: elements.originCompetitionName.value
-    }
+const addEventForm = document.getElementById("add-event-form");
+addEventForm.addEventListener("submit", onSubmitNewEvent);
+
+function onSubmitNewEvent(event) {
+  const newSportDataEvent = mapResultFromSubmitToObject(event.target.elements);
+
+  const {
+    season,
+    dateVenue,
+    originCompetitionName,
+    homeTeam,
+    awayTeam,
+    status,
+  } = newSportDataEvent;
+  const newSportEventTableRow = createTableRow({
+    season,
+    dateVenue,
+    originCompetitionName,
+    homeTeam: homeTeam.officialName,
+    awayTeam: awayTeam.officialName,
+    status,
+    index: eventState.length,
+  });
+
+  console.log(newSportEventTableRow, "newSportEventTableRow");
+
+  tableBody.appendChild(newSportEventTableRow);
+
+  addSportEvent(newSportDataEvent);
+
+  addEventForm.reset();
 }
-
-const onSubmitNewEvent = (event) => {
-
-    const newSportDataEvent = mapResultFromSubmitToObject(event.target.elements);
-
-    const { season, dateVenue, originCompetitionName, homeTeam, awayTeam, status } = newSportDataEvent;
-    const newSportEventTableRow = createTableRow({
-        season, dateVenue, originCompetitionName, homeTeam: homeTeam.officialName, awayTeam: awayTeam.officialName, status
-    });
-
-    addSportEvent(newSportDataEvent);
-}
-
-
-// TODO reuse createTableRow to contruct new tr
-
-
-
-
-const addEventForm = document.getElementById('add-event-form');
-addEventForm.addEventListener("submit", () => {
-    onSubmitNewEvent,
-        addEventForm.reset()
-});
--- templates/tableRow.js
+++ templates/tableRow.js
@@ -1,43 +1,51 @@
 const createEl = (element) => document.createElement(element);
 
+export const createTableRow = ({
+  season,
+  dateVenue,
+  originCompetitionName,
+  homeTeam,
+  awayTeam,
+  status,
+  index,
+}) => {
+  const tr = createEl("tr");
+  tr.setAttribute("data-id", index);
 
-export const createTableRow = ({ season, dateVenue, originCompetitionName, homeTeam, awayTeam, status, index }) => {
+  const tdSeason = createEl("td");
+  tdSeason.innerText = season;
 
-    const tr = createEl("tr");
-    tr.setAttribute("data-id", index);
+  const tdDate = createEl("td");
+  tdDate.innerText = dateVenue;
 
-    const tdSeason = createEl("td");
-    tdSeason.innerText = season;
+  const tdEvent = createEl("td");
+  tdEvent.innerText = originCompetitionName;
 
-    const tdDate = createEl("td");
-    tdDate.innerText = dateVenue;
+  const tdTeams = createEl("td");
 
-    const tdEvent = createEl("td");
-    tdEvent.innerText = originCompetitionName;
+  const pHomeTeam = createEl("p");
+  pHomeTeam.innerText = homeTeam;
 
+  const pAwayTeam = createEl("p");
+  pAwayTeam.innerText = awayTeam;
 
-    const tdTeams = createEl("td");
+  tdTeams.appendChild(pHomeTeam);
+  tdTeams.appendChild(pAwayTeam);
 
+  const tdStatus = createEl("td");
+  const spanStatus = createEl("span");
+  spanStatus.classList.add("badge");
+  spanStatus.classList.add(`${status}`);
+  spanStatus.innerText = status;
 
-    const pHomeTeam = createEl("p");
-    pHomeTeam.innerText = homeTeam;
-
-    const pAwayTeam = createEl("p");
-    pAwayTeam.innerText = awayTeam;
-
-    tdTeams.appendChild(pHomeTeam);
-    tdTeams.appendChild(pAwayTeam);
-
-    const tdStatus = createEl("td");
-    tdStatus.innerText = status;
+  tdStatus.appendChild(spanStatus);
 
 
-    tr.appendChild(tdSeason);
-    tr.appendChild(tdDate);
-    tr.appendChild(tdEvent);
-    tr.appendChild(tdTeams);
-    tr.appendChild(tdStatus);
+  tr.appendChild(tdSeason);
+  tr.appendChild(tdDate);
+  tr.appendChild(tdEvent);
+  tr.appendChild(tdTeams);
+  tr.appendChild(tdStatus);
 
-    return tr;
-
-}
+  return tr;
+};
